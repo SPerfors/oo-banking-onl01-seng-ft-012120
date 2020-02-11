72c2b18f82bec81a4cf7391613293433578719da
Steven Perfors <stevecanbuild@gmail.com>
2020-02-11 15:00:31 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status, :bank_account
 
+  def initialize(name)
+    @name = name
+    @balance = 1000 
+    @status = "open"
+    @bank_account = bank_account
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    valid? == @balance > 0 && @status == "open"
+  end
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  attr_accessor :transfer, :sender, :receiver, :status
+  
+  def initialize(sender)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer = ''
+  end
+  
+  
 end
